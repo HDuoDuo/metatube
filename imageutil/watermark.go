99a9f32d842f87fbddf3b210b2fc3198cabc2a12
Da Hai <me@dahai.com>
2025-01-25 19:30:36 +0800

    kk

--- imageutil/watermark.go
+++ imageutil/watermark.go
@@ -3,7 +3,7 @@ package imageutil
 import (
 	"image"
 	"image/draw"
-	scdraw "golang.org/x/image/draw"
+	dr "golang.org/x/image/draw"
 )
 
 func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
@@ -14,9 +14,9 @@ func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
 	width := int(float64(height) / float64(wmk.Bounds().Dy()) * float64(wmk.Bounds().Dx()))
 	rect := image.Rect(0, 0, width, height)
 	dsi := image.NewRGBA(rect)
-	sc := scdraw.BiLinear /* default interpolator */
+	sc := dr.BiLinear /* default interpolator */
 	// sc.Scale(dst, rect.Add(dst.Bounds().Max.Sub(Point{width, height})), wmk, wmk.Bounds(), scdraw.Over, nil)
-	sc.Scale(dsi, rect, wmk, wmk.Bounds(), scdraw.Over, nil)
+	sc.Scale(dsi, rect, wmk, wmk.Bounds(), dr.Over, nil)
 
 	// temp := imageutil.Resize(wmk, 0, wmk.Bounds().Dy()/2)
 	// draw.Draw(dst, temp.Bounds().Add(dst.Bounds().Max.Sub(temp.Bounds().Max)), temp, temp.Bounds().Min.Add(pt), draw.Over)
