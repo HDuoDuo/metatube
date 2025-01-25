e69672a192ee6be013c897a02b5f981020dc6727
Da Hai <me@dahai.com>
2025-01-25 16:11:54 +0800

    fix

--- imageutil/watermark.go
+++ imageutil/watermark.go
@@ -9,15 +9,15 @@ func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
 	dst := image.NewNRGBA(image.Rect(0, 0, src.Bounds().Dx(), src.Bounds().Dy()))
 	draw.Draw(dst, dst.Bounds(), src, src.Bounds().Min, draw.Src)
 
-	height = wmk.Bounds().Dy() * 0.55
+	// height = wmk.Bounds().Dy() * 0.55
 	// width = int(float64(height) / float64(wmk.Bounds().Dy()) * float64(wmk.Bounds().Dx()))
 	// rect := image.Rect(0, 0, width, height)
 	// temp := image.NewRGBA(rect)
 	// sc := scdraw.BiLinear /* default interpolator */
 	// sc.Scale(temp, rect, wmk, wmk.Bounds(), scdraw.Over, nil)
 
-	temp := imageutil.Resize(wmk, 0, height)
+	// temp := imageutil.Resize(wmk, 0, height)
 
-	draw.Draw(dst, temp.Bounds().Add(dst.Bounds().Max.Sub(temp.Bounds().Max)), temp, temp.Bounds().Min.Add(pt), draw.Over)
+	draw.Draw(dst, wmk.Bounds().Add(dst.Bounds().Max.Sub(temp.Bounds().Max)), wmk, wmk.Bounds().Min.Add(pt), draw.Over)
 	return dst
 }
