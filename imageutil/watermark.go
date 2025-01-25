1f36d589f498285c0e52e79503aa20a7707be4e8
Da Hai <me@dahai.com>
2025-01-25 21:29:15 +0800

    gg

--- imageutil/watermark.go
+++ imageutil/watermark.go
@@ -3,7 +3,7 @@ package imageutil
 import (
 	"image"
 	"image/draw"
-	// dr "golang.org/x/image/draw"
+	dr "golang.org/x/image/draw"
 	"github.com/HDuoDuo/metatube/imageutil"
 )
 
@@ -11,13 +11,13 @@ func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
 	dst := image.NewNRGBA(image.Rect(0, 0, src.Bounds().Dx(), src.Bounds().Dy()))
 	draw.Draw(dst, dst.Bounds(), src, src.Bounds().Min, draw.Src)
 
-	// height := int(float64(wmk.Bounds().Dy()/2))
-	// width := int(float64(height) / float64(wmk.Bounds().Dy()) * float64(wmk.Bounds().Dx()))
-	// rect := image.Rect(0, 0, width, height)
-	// // sc.Scale(dst, rect.Add(dst.Bounds().Max.Sub(Point{width, height})), wmk, wmk.Bounds(), scdraw.Over, nil)
+	height := int(float64(wmk.Bounds().Dy()/2))
+	width := int(float64(height) / float64(wmk.Bounds().Dy()) * float64(wmk.Bounds().Dx()))
+	rect := image.Rect(0, 0, width, height)
+	dr.BiLinear.Scale(dst, rect.Add(dst.Bounds().Max.Sub(rect.Size())), wmk, wmk.Bounds(), dr.Over, nil)
 	// dr.BiLinear.Scale(dst, rect, wmk, wmk.Bounds(), dr.Over, nil)
 
-	temp := imageutil.Resize(wmk, 0, wmk.Bounds().Dy()/2)
-	draw.Draw(dst, temp.Bounds().Add(dst.Bounds().Max.Sub(temp.Bounds().Max)), temp, temp.Bounds().Min.Add(pt), draw.Over)
+	// temp := imageutil.Resize(wmk, 0, wmk.Bounds().Dy()/2)
+	// draw.Draw(dst, temp.Bounds().Add(dst.Bounds().Max.Sub(temp.Bounds().Max)), temp, temp.Bounds().Min.Add(pt), draw.Over)
 	return dst
 }
