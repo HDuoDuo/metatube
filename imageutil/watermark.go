d76f46782ab44a63e9b6dca959559bdfab27eef4
Da Hai <me@dahai.com>
2025-01-25 19:36:01 +0800

    kk

--- imageutil/watermark.go
+++ imageutil/watermark.go
@@ -10,7 +10,7 @@ func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
 	dst := image.NewNRGBA(image.Rect(0, 0, src.Bounds().Dx(), src.Bounds().Dy()))
 	draw.Draw(dst, dst.Bounds(), src, src.Bounds().Min, draw.Src)
 
-	height := wmk.Bounds().Dy() * 0.5
+	height := int(wmk.Bounds().Dy() * 0.55)
 	width := int(float64(height) / float64(wmk.Bounds().Dy()) * float64(wmk.Bounds().Dx()))
 	rect := image.Rect(0, 0, width, height)
 	dsi := image.NewRGBA(rect)
