379210b21782e2ae05b3362af7d0ce2987d2c05d
Da Hai <me@dahai.com>
2025-01-25 16:35:26 +0800

    fix

--- imageutil/watermark.go
+++ imageutil/watermark.go
@@ -8,6 +8,7 @@ import (
 func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
 	dst := image.NewNRGBA(image.Rect(0, 0, src.Bounds().Dx(), src.Bounds().Dy()))
 	draw.Draw(dst, dst.Bounds(), src, src.Bounds().Min, draw.Src)
-	draw.Draw(dst, wmk.Bounds().Add(dst.Bounds().Max.Sub(wmk.Bounds().Max)), wmk, wmk.Bounds().Min.Add(pt), draw.Over)
+	temp := imageutil.Resize(wmk, 0, wmk.Bounds().Dy() * 0.55)
+	draw.Draw(dst, temp.Bounds().Add(dst.Bounds().Max.Sub(temp.Bounds().Max)), temp, temp.Bounds().Min.Add(pt), draw.Over)
 	return dst
 }
