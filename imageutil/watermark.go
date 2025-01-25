981ffd903f5e7f3f1379626761b48b3d8aee833c
Da Hai <me@dahai.com>
2025-01-25 20:28:00 +0800

    啦啦啦

--- imageutil/watermark.go
+++ imageutil/watermark.go
@@ -13,9 +13,8 @@ func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
 	height := int(float64(wmk.Bounds().Dy() * 0.55))
 	width := int(float64(height) / float64(wmk.Bounds().Dy()) * float64(wmk.Bounds().Dx()))
 	rect := image.Rect(0, 0, width, height)
-	sc := dr.BiLinear
 	// sc.Scale(dst, rect.Add(dst.Bounds().Max.Sub(Point{width, height})), wmk, wmk.Bounds(), scdraw.Over, nil)
-	sc.Scale(dst, rect, wmk, wmk.Bounds(), dr.Over, nil)
+	dr.BiLinear.Scale(dst, rect, wmk, wmk.Bounds(), dr.Over, nil)
 
 	// temp := imageutil.Resize(wmk, 0, wmk.Bounds().Dy()/2)
 	// draw.Draw(dst, temp.Bounds().Add(dst.Bounds().Max.Sub(temp.Bounds().Max)), temp, temp.Bounds().Min.Add(pt), draw.Over)
