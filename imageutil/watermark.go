10af9c26dc27179f2ab382e48aac62bcbb4344fb
Da Hai <me@dahai.com>
2025-01-25 16:07:04 +0800

    fix

--- imageutil/watermark.go
+++ imageutil/watermark.go
@@ -3,7 +3,6 @@ package imageutil
 import (
 	"image"
 	"image/draw"
-	scdraw "golang.org/x/image/draw"
 )
 
 func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
@@ -11,11 +10,13 @@ func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
 	draw.Draw(dst, dst.Bounds(), src, src.Bounds().Min, draw.Src)
 
 	height = wmk.Bounds().Dy() * 0.55
-	width = int(float64(height) / float64(wmk.Bounds().Dy()) * float64(wmk.Bounds().Dx()))
-	rect := image.Rect(0, 0, width, height)
-	temp := image.NewRGBA(rect)
-	sc := scdraw.BiLinear /* default interpolator */
-	sc.Scale(temp, rect, wmk, wmk.Bounds(), scdraw.Over, nil)
+	// width = int(float64(height) / float64(wmk.Bounds().Dy()) * float64(wmk.Bounds().Dx()))
+	// rect := image.Rect(0, 0, width, height)
+	// temp := image.NewRGBA(rect)
+	// sc := scdraw.BiLinear /* default interpolator */
+	// sc.Scale(temp, rect, wmk, wmk.Bounds(), scdraw.Over, nil)
+
+	temp := imageutil.Resize(wmk, 0, height)
 
 	draw.Draw(dst, temp.Bounds().Add(dst.Bounds().Max.Sub(temp.Bounds().Max)), temp, temp.Bounds().Min.Add(pt), draw.Over)
 	return dst
