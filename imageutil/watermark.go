747c12f1a385999ec57ca20ce245e762ec310314
Da Hai <me@dahai.com>
2025-01-25 22:34:16 +0800

    hh

--- imageutil/watermark.go
+++ imageutil/watermark.go
@@ -7,15 +7,15 @@ import (
 )
 
 func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
-	dst := image.NewRGBA(image.Rect(0, 0, src.Bounds().Dx(), src.Bounds().Dy()))
-	draw.Draw(dst, dst.Bounds(), src, src.Bounds().Min, draw.Src)
-
 	height := int(float64(wmk.Bounds().Dy()/2))
 	width := int(float64(height) / float64(wmk.Bounds().Dy()) * float64(wmk.Bounds().Dx()))
-	rect := image.Rect(dst.Bounds().Dx() - width, dst.Bounds().Dy() - height, width, height)
-	dr.BiLinear.Scale(dst, rect, wmk, wmk.Bounds(), dr.Over, nil)
+	rect := image.Rect(src.Bounds().Dx() - width, src.Bounds().Dy() - height, width, height)
+	dr.BiLinear.Scale(src, rect, wmk, wmk.Bounds(), dr.Over, nil)
 	// dr.BiLinear.Scale(dst, rect.Add(src.Bounds().Max.Sub(rect.Size())), wmk, wmk.Bounds(), dr.Over, nil)
 	// dr.BiLinear.Scale(dst, rect, wmk, wmk.Bounds(), dr.Over, nil)
+
+	dst := image.NewNRGBA(image.Rect(0, 0, src.Bounds().Dx(), src.Bounds().Dy()))
+	draw.Draw(dst, dst.Bounds(), src, src.Bounds().Min, draw.Src)
 
 	// temp := imageutil.Resize(wmk, 0, wmk.Bounds().Dy()/2)
 	// draw.Draw(dst, temp.Bounds().Add(dst.Bounds().Max.Sub(temp.Bounds().Max)), temp, temp.Bounds().Min.Add(pt), draw.Over)
