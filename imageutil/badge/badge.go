a90f3a753108985adc1154e8d47f4e01172a446b
Da Hai <me@dahai.com>
2025-01-25 10:56:06 +0800

    fix

--- imageutil/badge/badge.go
+++ imageutil/badge/badge.go
@@ -42,7 +42,5 @@ func Badge(src image.Image, badge string) (image.Image, error) {
 		badgeCache.Set(badge, img, ttlcache.DefaultTTL)
 	}
 	wmk := imageutil.Resize(img, 0, src.Bounds().Dy()/5 /* 0.2 */)
-	wmkX := src.Bounds().Dx() - wmk.Bounds().Dx()
-	wmkY := src.Bounds().Dy() - wmk.Bounds().Dy()
-	return imageutil.Watermark(src, wmk, image.Point{X: wmkX, Y: wmkY}), nil
+	return imageutil.Watermark(src, wmk, image.Point{}), nil
 }
