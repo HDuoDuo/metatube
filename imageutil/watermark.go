c71fb58788336a9eb4bfb37e8dee15e6322f233e
Da Hai <me@dahai.com>
2025-01-25 15:03:35 +0800

    fix

--- imageutil/watermark.go
+++ imageutil/watermark.go
@@ -3,12 +3,18 @@ package imageutil
 import (
 	"image"
 	"image/draw"
+	"golang.org/x/image/draw"
 )
 
 func Watermark(src image.Image, wmk image.Image, pt image.Point) image.Image {
 	dst := image.NewNRGBA(image.Rect(0, 0,
 		src.Bounds().Dx(), src.Bounds().Dy()))
 	draw.Draw(dst, dst.Bounds(), src, src.Bounds().Min, draw.Src)
-	draw.Draw(dst, wmk.Bounds().Add(dst.Bounds().Max.Sub(wmk.Bounds().Max.Div(2))), wmk, wmk.Bounds().Min.Add(pt), draw.Over)
+
+	height = wmk.Bounds().Dy() * 0.56
+	width = int(float64(height) / float64(wmk.Bounds().Dy()) * float64(wmk.Bounds().Dx()))
+	rect := image.Rect(0, 0, width, height)
+	sc := draw.BiLinear /* default interpolator */
+	sc.Scale(dst, rect.Bounds().Add(dst.Bounds().Max.Sub(rect.Bounds().Max)), wmk, wmk.Bounds(), draw.Over, nil)
 	return dst
 }
